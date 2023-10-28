3e89e888661b14e8c8af3fdb64db46bac50da20a
John Langs <johnslangs@tamu.edu>
2023-10-28 17:44:21 -0500

    Added loading option

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Categories from "./Categories";
-import { Container, Grid } from "@mui/material";
+import { Box, CircularProgress, Container, Grid } from "@mui/material";
 import { getDrinksWithinCategories } from "@/lib/orderQueries";
 import { useOrder } from "./OrderContext";
 import { drinks } from "@prisma/client";
@@ -32,13 +32,24 @@ export default function Page() {
         getMenu();
     }, []);
 
-    return (
+    return !loading ? (
         <main>
             <Container style={{alignItems:'center', justifyContent:'center'}}>
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} style={{padding: '1rem',}}>
                     <Categories categories={Array.from(menu.keys())}></Categories>
                 </Grid>
             </Container>
+        </main>
+    ) : (
+        <main>
+            <Box 
+            sx={{ display: 'flex' }}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh">
+                <CircularProgress sx={{color: 'red'}}/>
+            </Box>
         </main>
     )
 }
