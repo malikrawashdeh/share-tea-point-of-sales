8dd71b037e9892116d8624c45dfa125a591540a9
John Langs <me@johnlangs.net>
2023-11-01 15:37:16 -0500

    deploy fix

--- app/employees/manager/chart.tsx
+++ app/employees/manager/chart.tsx
@@ -1,26 +1,26 @@
-import { useEffect, useState } from "react";
-import { BarChart } from "@/node_modules/@mui/icons-material/index";
-import React from "react";
+// import { useEffect, useState } from "react";
+// import { BarChart } from "@/node_modules/@mui/icons-material/index";
+// import React from "react";
 
-export default function Chart() {
-    const [sales, setSales] = useState(null);
-    const [loading, setLoading] = useState(true);
+// export default function Chart() {
+//     const [sales, setSales] = useState(null);
+//     const [loading, setLoading] = useState(true);
 
-    const getSalesData = React.useCallback(async () => {
-        setLoading(true);
-        const res = await fetch("/api/getDataRange");
-        const data = await res.json();
-        setSales(data);
-        setLoading(false);
-    }, []);
+//     const getSalesData = React.useCallback(async () => {
+//         setLoading(true);
+//         const res = await fetch("/api/getDataRange");
+//         const data = await res.json();
+//         setSales(data);
+//         setLoading(false);
+//     }, []);
 
-    useEffect(() => {
-        getSalesData();
-    }, []);
+//     useEffect(() => {
+//         getSalesData();
+//     }, []);
 
-    return (
-        {
-            !loading && sales != null ? 
-        }
-    );
-}
+//     return (
+//         {
+//             !loading && sales != null ? 
+//         }
+//     );
+// }
