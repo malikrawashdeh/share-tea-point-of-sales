8ffeb6a0fc7c4662e5c5c0407e4c9baeb6428bb8
fr713 <farrisrizwan@gmail.com>
2023-11-23 20:21:35 -0600

    addition of ingredients prisma

---
+++ app/employees/manager/ingredients/IngredientsTable.tsx
@@ -0,0 +1,6 @@
+'use client'
+
+import { TableContainer, Table, TableHead, TableCell, TableRow, TableBody, Paper } from "@mui/material";
+import { ingredients } from "@prisma/client";
+import React from "react";
+
