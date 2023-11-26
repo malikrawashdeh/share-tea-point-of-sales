d23fa5a36b436f6a4c9effa82444df5aa68b20dd
fr713 <farrisrizwan@gmail.com>
2023-11-25 20:11:34 -0600

    Ingredients interface created

--- app/employees/manager/ingredients/IngredientsTable.tsx
+++ app/employees/manager/ingredients/IngredientsTable.tsx
@@ -4,3 +4,7 @@ import { TableContainer, Table, TableHead, TableCell, TableRow, TableBody, Paper
 import { ingredients } from "@prisma/client";
 import React from "react";
 
+interface IngredientProps {
+    ingredients: { id: number; name: string | null; quantity: number | null; unit_price: string | null; min_quantity: number | null;}[],
+    changeIngredient(newIngredient: ingredients): void
+}
