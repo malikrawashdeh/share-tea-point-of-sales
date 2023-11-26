5db3a3d2763b3eff0cdca6e98a73bf40fd53163f
fr713 <farrisrizwan@gmail.com>
2023-11-25 20:22:44 -0600

    table displaying ingredients has been created

--- app/employees/manager/ingredients/IngredientsTable.tsx
+++ app/employees/manager/ingredients/IngredientsTable.tsx
@@ -8,3 +8,39 @@ interface IngredientProps {
     ingredients: { id: number; name: string | null; quantity: number | null; unit_price: string | null; min_quantity: number | null;}[],
     changeIngredient(newIngredient: ingredients): void
 }
+
+
+const IngredientsTable: React.FC<IngredientProps> = ({ingredients, changeIngredient}) => {
+    return ingredients.length > 0 ? (
+            <TableContainer component={Paper} sx={{padding: '1rem'}}>
+                <Table sx={{  }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="right">Id</TableCell>
+                            <TableCell align="right">Name</TableCell>
+                            <TableCell align="right">Quantity</TableCell>
+                            <TableCell align="right">Unit Price</TableCell>
+                            <TableCell align="right">Min Quantity</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {ingredients.map((row) => (
+                            <TableRow
+                            onClick = {() => changeIngredient(row)}
+                            key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                            <TableCell align="right">{row.id}</TableCell>
+                            <TableCell align="right">{row.name}</TableCell>
+                            <TableCell align="right">{row.quantity}</TableCell>
+                            <TableCell align="right">{"$" + row.unit_price}</TableCell>
+                            <TableCell align="right">{row.min_quantity}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+    ) : (
+        <div>No Ingredients Found</div>
+    );
+}
+
+export default IngredientsTable;
