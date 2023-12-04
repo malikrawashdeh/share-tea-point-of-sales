ef9356e119bdb587ab8149d03ada4ebcf3ac681e
Malik Rawashdeh <malikrawashdeh5@gmail.com>
2023-12-04 15:22:53 -0600

    added link

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.json",
+    url: "https://csce-315-331-2023c.github.io/project-3-903_03b-taele-s-dream-team/swagger.json",
     dom_id: "#swagger-ui",
     deepLinking: true,
     presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
