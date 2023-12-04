a1001d5f7826f6b5b6932a9731c161af51981f47
John Langs <me@johnlangs.net>
2023-12-03 20:32:50 -0600

    docs dev up

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -2,13 +2,16 @@ import { getApiDocs } from "@/lib/swagger";
 import ReactSwagger from "./react-swagger";
 import Card from "@mui/material/Card";
 
+import dynamic from "next/dynamic";
+const SwaggerUI = dynamic(import('swagger-ui-react'), {ssr: false})
+
 export default async function IndexPage() {
   const spec = await getApiDocs();
   return (
     <section className="container">
       <Card>
         <h1>API Documentation</h1>
-        <ReactSwagger spec={spec} />
+        <SwaggerUI spec={spec} />
       </Card>
     </section>
   );
--- next.config.js
+++ next.config.js
@@ -2,12 +2,3 @@
 const nextConfig = {}
 
 module.exports = nextConfig
-
-webpack: config => {
-    config.ignoreWarnings = [
-      {module: /node_modules\/swagger-jsdoc\/src\/utils\.js/},
-      {file: /node_modules\/swagger-jsdoc\/src\/index\.js/},
-    ];
-
-    return config;
-  }
